2ca5cab16ed5e35f296f77df824bedcdad998374
Rob Cheung <kousun12@gmail.com>
2015-08-22 22:59:13 -0700

    add example project

---
+++ FinImagePickerExample/FinImagePickerExample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FinImagePickerExample
+//
+//  Created by Rob Cheung on 8/22/15.
+//  Copyright (c) 2015 Fin Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var image: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func showChooser(sender: AnyObject) {
+        
+    }
+}
+
